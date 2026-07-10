5d4a3294fa9672f65b6ebd9701c0abcaffdeb4cf
Billy Blair <billy.blair@gmail.com>
2026-07-10 11:33:38 +0100

    Update to 0.3. Add image of Windows folder layout.

--- platform/android/app/build.gradle.kts
+++ platform/android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.bassquake.avpvr"
         minSdk = 24
         targetSdk = 32
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

0311042db32513049931814859bdc7b14f7e4cde
Billy Blair <billy.blair@gmail.com>
2026-08-01 19:10:50 +0100

    Update to 0.5 versioning.

--- platform/android/app/build.gradle.kts
+++ platform/android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.bassquake.avpvr"
         minSdk = 24
         targetSdk = 32
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
